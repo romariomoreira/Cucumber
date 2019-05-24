3170d222e8672bff16cb0958a929ad3b60893c81
Romario Silva <romariomoreiradasilva@hotmail.com>
2019-05-24 00:04:15 -0300

    Criando testes com cucumber

---
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.feature
@@ -0,0 +1,16 @@
+#language: pt
+
+Funcionalidade: Aprender Cucumber
+Como um aluno 
+Eu quero aprrender a aultilizar Cucumber
+Para que eu possa automatizar crit�rios de aceita��o 
+
+
+Cenário: Deve executar especificação 
+
+	Dado que criei o arquivo corretamente
+	Quando executá-lo 
+	Então a especificação deve finalizar com sucesso
+
+
+
---
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.java
@@ -0,0 +1,23 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Aprender_Cucumber {
+
+	@Given("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+			
+	}
+
+	@When("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	    
+	}
+
+	@Then("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	  
+	}
+
+}
