acfd3d65aab40c847460e75dff9d8420127fe4ac
Romario Silva <romariomoreiradasilva@hotmail.com>
2019-05-24 00:41:09 -0300

    Criando testes com cucumber

--- Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.feature
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.feature
@@ -11,6 +11,11 @@ Cenário: Deve executar especificação
 	Dado que criei o arquivo corretamente
 	Quando executá-lo 
 	Então a especificação deve finalizar com sucesso
+		
+Cenário: Deve icrementar contador 
+	Dado que o valor do contador é 15 
+	Quando eu incrementar em 5 
+	Então o valor do contador será 18
 
 
 
--- Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.java
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.java
@@ -1,23 +1,51 @@
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+import sun.java2d.pipe.hw.AccelGraphicsConfig;
+
+
+
+
 
 public class Aprender_Cucumber {
 
-	@Given("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-			
-	}
-
-	@When("^executá-lo$")
-	public void executá_lo() throws Throwable {
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente() throws Throwable {
 	    
 	}
 
-	@Then("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-	  
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
+	 
 	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	    
+	}
+	
+private int contador = 0 ; 	
+
+@Dado("^que o valor do contador é (\\d+)$")
+public void que_o_valor_do_contador_é(int arg1) throws Throwable {
+	
+	contador = arg1 ; 
+    
+}
+
+@Quando("^eu incrementar em (\\d+)$")
+public void eu_incrementar_em(int arg1) throws Throwable {
+	contador = contador + arg1 ;  
+    
+}
+
+@Então("^o valor do contador será (\\d+)$")
+public void o_valor_do_contador_será(int arg1) throws Throwable {
+   System.out.println(arg1);
+   System.out.println(contador);
+   System.out.println(arg1 == contador);
+}
+
+
 
 }
