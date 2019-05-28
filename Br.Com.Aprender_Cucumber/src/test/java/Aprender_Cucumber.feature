a558aef877df7ea9e518e233850228e186338816
Romario Silva <romariomoreiradasilva@hotmail.com>
2019-05-28 01:01:42 -0300

    Cucumber fazendo varios cenários

--- Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.feature
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.feature
@@ -12,10 +12,48 @@ Cenário: Deve executar especificação
 	Quando executá-lo 
 	Então a especificação deve finalizar com sucesso
 		
-Cenário: Deve icrementar contador 
-	Dado que o valor do contador é 15 
-	Quando eu incrementar em 5 
+Cenário: Deve icrementar contador 1
+	Dado que o valor do contador é 15
+	Quando eu incrementar em 3
 	Então o valor do contador será 18
 
+Cenário: Deve icrementar contador 2
+	Dado que o valor do contador é 123 
+	Quando eu incrementar em 35 
+	Então o valor do contador será 158
 
+Cenário: Deve Calclular atraso na entregra
+	Dado que a entrega é dia 05/04/2018
+	Quando eu atrasar em 2 dias
+	Então a entrga será efetuada em 07/04/2018
+	
+	
+	Cenário: Deve Calclular atraso ma entrega da china
+	Dado que a entrega é dia 05/04/2018
+	Quando eu atrasar em 2 meses 
+	Então a entrga será efetuada em 05/06/2018
+	
+	
+	
+	Cenário: Deve criar steps genéricos para estes passos
+    Dado que o ticket é AF345
+    Dado que o valor da passagem é R$ 230,45
+    Dado que o nome do passageiro é "Fulano da Silva"
+    Dado que o telefone do passageiro é 9999-9999
+    Quando criar os steps
+    Então o teste vai funcionar
 
+Cenário: Deve reaproveitar os steps "Dado" do cenário anterior
+    Dado que o ticket é AB167
+    Dado que o ticket especial é AB167
+    Dado que o valor da passagem é R$ 1120,23
+    Dado que o nome do passageiro é "Cicrano de Oliveira"
+    Dado que o telefone do passageiro é 9888-8888
+
+Cenário: Deve negar todos os steps "Dado" dos cenários anteriores
+    Dado que o ticket é CD123
+    Dado que o ticket é AG1234
+    Dado que o valor da passagem é R$ 1.1345,56
+    Dado que o nome do passageiro é "Beltrano Souza Matos de Alcântara Azevedo"
+    Dado que o telefone do passageiro é 1234-5678
+    Dado que o telefone do passageiro é 999-2223 
--- Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.java
+++ Br.Com.Aprender_Cucumber/src/test/java/Aprender_Cucumber.java
@@ -1,51 +1,116 @@
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+
+import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Ent„o;
 import cucumber.api.java.pt.Quando;
-import sun.java2d.pipe.hw.AccelGraphicsConfig;
-
-
-
-
 
 public class Aprender_Cucumber {
 
 	@Dado("^que criei o arquivo corretamente$")
 	public void queCrieiOArquivoCorretamente() throws Throwable {
-	    
+
 	}
 
 	@Quando("^execut·-lo$")
 	public void execut·Lo() throws Throwable {
-	 
+
 	}
 
 	@Ent„o("^a especificaÁ„o deve finalizar com sucesso$")
 	public void aEspecificaÁ„oDeveFinalizarComSucesso() throws Throwable {
+
+	}
+
+	private int contador = 0;
+
+	@Dado("^que o valor do contador È (\\d+)$")
+	public void que_o_valor_do_contador_È(int arg1) throws Throwable {
+
+		contador = arg1;
+
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void eu_incrementar_em(int arg1) throws Throwable {
+		contador = contador + arg1;
+
+	}
+
+	@Ent„o("^o valor do contador ser· (\\d+)$")
+	public void o_valor_do_contador_ser·(int arg1) throws Throwable {
+		Assert.assertEquals(arg1, contador);
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega È dia (\\d+)/(\\d+)/(\\d+)$")
+	public void que_a_entrega_È_dia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+
+	}
+
+	@Quando("^eu atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void euAtrasarEmDias(int arg1, String tempo) throws Throwable {
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if (tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if (tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Ent„o("^a entrga ser· efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void a_entrga_ser·_efetuada_em(String data) throws Throwable {
+		DateFormat formart = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = formart.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
+	}
+
+	@Dado("^que o ticket( especial)? È (A.\\d{3})$")
+	public void queOTicket…AF(String tipo, String arg1) throws Throwable {
 	    
 	}
+
+	@Dado("^que o valor da passagem È R\\$ (\\d+),(\\d+)$")
+	public void queOValorDaPassagem…R$(int arg1, int arg2) throws Throwable {
+	    
+	}
+
+	@Dado("^que o nome do passageiro È \"(.{5,20})\"$")
+	public void queONomeDoPassageiro…(String arg1) throws Throwable {
+	    
+	}
+
+	@Dado("^que o telefone do passageiro È (9\\d{3}-\\d{4})$")
+	public void queOTelefoneDoPassageiro…(String telefone) throws Throwable {
+	   
+	}
+
+	@Quando("^criar os steps$")
+	public void criarOsSteps() throws Throwable {
+	  
+	}
+
+	@Ent„o("^o teste vai funcionar$")
+	public void oTesteVaiFuncionar() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 	
-private int contador = 0 ; 	
-
-@Dado("^que o valor do contador È (\\d+)$")
-public void que_o_valor_do_contador_È(int arg1) throws Throwable {
 	
-	contador = arg1 ; 
-    
-}
-
-@Quando("^eu incrementar em (\\d+)$")
-public void eu_incrementar_em(int arg1) throws Throwable {
-	contador = contador + arg1 ;  
-    
-}
-
-@Ent„o("^o valor do contador ser· (\\d+)$")
-public void o_valor_do_contador_ser·(int arg1) throws Throwable {
-   System.out.println(arg1);
-   System.out.println(contador);
-   System.out.println(arg1 == contador);
-}
-
-
-
 }
